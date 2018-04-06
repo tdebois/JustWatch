28dd46b5c778578d9e05d079be9037a96e42b358
Alice1017 <genocidedragon@gmail.com>
2018-04-06 11:29:30 -0400

    write: import modules

--- justwatch/__init__.py
+++ justwatch/__init__.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from justwatch.manager import WatchManager
+from justwatch.objects import FileItem
+from justwatch.observer import Observer
+
 __author__ = "Hayato Tominaga"
 __version__ = "1.0.0a1"
+__all__ = ["WatchManager", "FileItem", "Observer"]
