069bf2a1b30015846fa0fed5b4a1b47b88efd114
paulolimac <paulolimac@gmail.com>
2018-10-31 17:48:36 -0300

    Add docstrings.
    Base from [PEP 257](https://www.python.org/dev/peps/pep-0257/#one-line-docstrings).
    
    Address issue #3.

--- justwatch/objects.py
+++ justwatch/objects.py
@@ -10,9 +10,10 @@ ISOFORMAT = "%Y/%m/%d/ - %H:%M:%S"
 
 
 class FileItem(object):
+    """Class to model a file."""
 
     def __init__(self, path):
-
+        """Initiate the class by the file path."""
         self.path = path
 
         self.stat = os.stat(path)
@@ -24,14 +25,14 @@ class FileItem(object):
         self.hash = self._get_hash(path)
 
     def _get_hash(self, path):
-
+        """Generate a hash from a path."""
         with open(path, "r") as fp:
             content = fp.read()
 
         return sha256(content).hexdigest()
 
     def __eq__(self, other):
-
+        """Verify if a file is equal or different from another file."""
         if (self.timestamp == other.timestamp) and (self.hash == other.hash):
             return True
 
@@ -39,7 +40,7 @@ class FileItem(object):
             return False
 
     def __ne__(self, other):
-
+        """Verify if a new file is different froma another file."""
         if (self.timestamp != other.timestamp) and (self.hash != other.hash):
             return True
 
@@ -47,5 +48,5 @@ class FileItem(object):
             return False
 
     def __repr__(self):
-
+        """Return the official string representation of the object."""
         return "<FileItem path='{0}'>".format(self.path)
