68a327a9086a03dc2bd6018fb8697bc2417a993e
Alice1017 <genocidedragon@gmail.com>
2019-03-21 00:37:06 +0900

    FEAT: yaspin spinner

--- justwatch/observer.py
+++ justwatch/observer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import sys
 import time
 
+from yaspin import yaspin
 from justwatch.objects import FileItem
 
 
@@ -19,6 +21,20 @@ class Observer(object):
 
     def watch(self):
         """Watch the file modifications."""
+
+        spinner = yaspin(text="Watching...", color="cyan")
+        try:
+            spinner.start()
+            self.__watch(spinner)
+
+        except KeyboardInterrupt:
+            sys.stderr.write("\nCought KeyboardInterrupt.\n")
+            spinner.stop()
+            sys.exit(0)
+        return
+
+    def __watch(self, spinner):
+
         while True:
 
             for index, item in enumerate(self.manager.files_container):
@@ -27,7 +43,9 @@ class Observer(object):
 
                 if item != new_item:
 
+                    spinner.hide()
                     self.callback(new_item)
+                    spinner.show()
                     self.manager.files_container[index] = new_item
 
             time.sleep(0.1)
